353c8059946a63bfd7056f535045f0e1d2be1ae6
Muhammad Hewedy <mhewedy@gmail.com>
2017-07-17 23:31:09 +0300

    init

---
+++ src/main/java/DbHelper.java
@@ -0,0 +1,32 @@
+import org.skife.jdbi.v2.Handle;
+
+import java.util.List;
+
+class DbHelper {
+
+    static void insertTagIfNotExists(Handle handle, String tag) {
+        handle.execute("insert into tag (tag)  select '" + tag
+                + "' from dual where not exists (select * from tag where tag='" + tag + "')");
+    }
+
+    static void insertSeries(Handle handle, Main.Series series) {
+        handle.insert("insert into series (sid, title, poster_url) values (?, ?, ?)",
+                series.sid, series.title, series.posterUrl);
+    }
+
+    static void linkSeriesWithTags(Handle handle, String seriesSid, List<String> tags) {
+        Object seriesId = handle.select("select id from series where sid = ?", seriesSid).get(0).get("id");
+
+        for (String tag : tags) {
+            Object tagId = handle.select("select id from tag where tag = ?", tag).get(0).get("id");
+            handle.insert("insert into series_tag (series_id, tag_id) values (?, ?)", seriesId, tagId);
+        }
+    }
+
+    static void insertEpisode(Handle handle, String seriesSid, Main.Episode episode) {
+        Object seriesId = handle.select("select id from series where sid = ?", seriesSid).get(0).get("id");
+
+        handle.insert("insert into episode (sid, video_url, duration_seconds, series_id) values" +
+                " (?, ?, ?, ?)", episode.sid, episode.videoUrl, episode.durationSeconds, seriesId);
+    }
+}
---
+++ src/main/resources/install.sql
@@ -0,0 +1,36 @@
+-- MYSQL
+
+CREATE DATABASE shahid CHARACTER SET utf8 COLLATE utf8_general_ci;
+
+USE shahid;
+
+CREATE TABLE tag(
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  tag   VARCHAR(50)
+);
+
+
+CREATE TABLE series(
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  sid   VARCHAR(50),
+  title VARCHAR(250),
+  poster_url	VARCHAR(4000)
+);
+
+
+CREATE TABLE series_tag(
+   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+   series_id	int,
+   tag_id	int
+);
+
+
+CREATE TABLE episode
+(
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  sid   VARCHAR(50),
+  video_url VARCHAR(250),
+  duration_seconds	int,
+  series_id	int
+);
+
