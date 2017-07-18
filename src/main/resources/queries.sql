05ee88bd1e555e9b5a1be6c1494944ebb1a744e7
Muhammad Hewedy <daziplqa@gmail.com>
2017-07-18 08:07:29 +0300

    Create queries.sql

---
+++ src/main/resources/queries.sql
@@ -0,0 +1,5 @@
+-- find series with missing video url for one or more of its episodes
+select series.title, tag.tag from series join episode on series.id = episode.series_id join series_tag on series.id = series_tag.series_id join tag on tag.id = series_tag.tag_id where episode.video_url is null;
+
+-- find series that have one or more episodes not inserted (may be all getToc return zero list)
+select * from series left outer join episode on series.id = episode.series_id where episode.id is null
